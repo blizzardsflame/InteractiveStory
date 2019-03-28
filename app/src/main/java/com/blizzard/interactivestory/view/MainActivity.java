7e4964350d33d323faaf78130ad8a45b700a562c
blizzardsflame <44416428+blizzardsflame@users.noreply.github.com>
2019-03-28 01:12:28 +0100

    Initial commit

---
+++ app/src/main/java/com/blizzard/interactivestory/view/MainActivity.java
@@ -0,0 +1,62 @@
+package com.blizzard.interactivestory.view;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.blizzard.interactivestory.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField;
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameField = (EditText)findViewById(R.id.nameEditText);
+        startButton = (Button)findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameField.getText().toString();
+                startStory(name);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nameField.setText("");
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        Resources resources = getResources();
+        String key = resources.getString(R.string.key_name);
+        intent.putExtra(key, name);
+        startActivity(intent);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/blizzard/interactivestory/view/StoryActivity.java
@@ -0,0 +1,130 @@
+package com.blizzard.interactivestory.view;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.blizzard.interactivestory.R;
+import com.blizzard.interactivestory.model.Page;
+import com.blizzard.interactivestory.model.Story;
+
+import java.util.Stack;
+
+public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private String name;
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+    private Stack<Integer> pageStack = new Stack<Integer>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        storyImageView = (ImageView)findViewById(R.id.storyImageView);
+        storyTextView = (TextView)findViewById(R.id.storyTextView);
+        choice1Button = (Button)findViewById(R.id.choice1Button);
+        choice2Button = (Button)findViewById(R.id.choice2Button);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra(getString(R.string.key_name));
+        if (name == null || name.isEmpty()) {
+            name = "Friend";
+        }
+        Log.d(TAG, name);
+
+        story = new Story();
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+        pageStack.push(pageNumber);
+
+        final Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
+
+        String pageText = getString(page.getTextId());
+        // Add name if placeholder included. Won't add if not
+        pageText = String.format(pageText, name);
+        storyTextView.setText(pageText);
+
+        if (page.isFinalPage()) {
+            choice1Button.setVisibility(View.INVISIBLE);
+            choice2Button.setText(R.string.play_again_button_text);
+            choice2Button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loadPage(0);
+                }
+            });
+        }
+        else {
+            loadButtons(page);
+        }
+    }
+
+    private void loadButtons(final Page page) {
+        choice1Button.setVisibility(View.VISIBLE);
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = page.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+
+        choice2Button.setVisibility(View.VISIBLE);
+        choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        pageStack.pop();
+        if (pageStack.isEmpty()) {
+            super.onBackPressed();
+        }
+        else {
+            loadPage(pageStack.pop());
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
